28cc93d03123eae976dd370a63f5896f68006c64
leilarodriguez-3103 <leilarodriguez3103@gmail.com>
2024-09-22 02:15:05 -0300

    eventos

---
+++ eventos.js
@@ -0,0 +1,3 @@
+document.getElementById('buttonContainer').addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
--- eventtos.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('buttonContainer').addEventListener('click', function() {
-    alert('Hola! Soy el div');
-});
